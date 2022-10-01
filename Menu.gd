c0b84b88522ba24cdbbc796f3d02fe759038bc7f
Lucas Nogueira <lucasnogueira@Lucass-MacBook-Pro.local>
2022-10-01 16:50:13 +0100

    Add start menu

---
+++ Menu.gd
@@ -0,0 +1,14 @@
+extends MarginContainer
+
+func _ready():
+	$VBoxContainer/StartButton.grab_focus()
+
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://fullmap.tscn")
+	
+func _on_QuitButton_pressed():
+	get_tree().quit()
+
+
+func _on_HelpButton_pressed():
+	get_tree().change_scene("res://Help.tscn")
