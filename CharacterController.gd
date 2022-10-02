43aae9ed9bb994eb02357735bda264cc73acb5e1
Dan Corder <dev@dancorder.com>
2022-10-02 22:54:37 +0100

    feat: keep ghost opposite of main sprite

--- CharacterController.gd
+++ CharacterController.gd
@@ -23,6 +23,8 @@ var coyote_timer = 0.0;
 var tile_shape;
 var collidable_container;
 var collision_shape;
+onready var main_sprite = $AnimatedSprite
+onready var ghost_sprite = $Ghost
 
 var walk_animation = "right-angel"
 var jump_animation = "jump-angel"
@@ -36,6 +38,7 @@ func _ready():
 	collidable_container = get_parent().get_node("collideableTiles");
 	tile_shape = collidable_container.shape_owner_get_shape(0, 0);
 	tilemaps = get_tilemap_children(collidable_container)
+	to_angel()
 
 func get_tilemap_children(parent_node):
 	var children = parent_node.get_children();
@@ -77,12 +80,12 @@ func _process(delta):
 		handle_falling(!Input.is_action_pressed("jump"), Input.is_action_pressed("move_down"), delta);
 
 	if velocity.y != 0:
-		$AnimatedSprite.play(jump_animation)
+		main_sprite.play(jump_animation)
 	elif velocity.x != 0:
-		$AnimatedSprite.play(walk_animation)
-		$AnimatedSprite.flip_h = velocity.x < 0
+		main_sprite.play(walk_animation)
+		main_sprite.flip_h = velocity.x < 0
 	elif velocity.x == 0:
-		$AnimatedSprite.stop()
+		main_sprite.stop()
 
 	# Move player
 	update();
@@ -92,17 +95,20 @@ func _process(delta):
 func to_angel():
 	walk_animation = "right-angel"
 	jump_animation = "jump-angel"
-	$AnimatedSprite.play(jump_animation)
-	$AnimatedSprite.stop()
+	main_sprite.play(jump_animation)
+	main_sprite.stop()
+	ghost_sprite.play("jump-devil")
+	ghost_sprite.stop()
 
 func to_devil():
 	walk_animation = "right-devil"
 	jump_animation = "jump-devil"
-	$AnimatedSprite.play(jump_animation)
-	$AnimatedSprite.stop()
+	main_sprite.play(jump_animation)
+	main_sprite.stop()
+	ghost_sprite.play("jump-angel")
+	ghost_sprite.stop()
 
 func kill():
-	print_debug("dead!")
 	emit_signal("playerDied")
 
 func handle_falling(fast_fall, hyper_fall, delta):
